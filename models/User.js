cc2d96be65b204050ed73586c1a956dc9b91bb5b
Venkatesh Dharavath <venkateshdharavath003@gmail.com>
2021-02-02 23:51:09 +0530

    Models Update

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.Now,
+  },
+  wishlist: [
+    {
+      product: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'product',
+      },
+    },
+  ],
+  history: [
+    {
+      product: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'order',
+      },
+    },
+  ],
+});
